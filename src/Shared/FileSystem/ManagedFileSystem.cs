5b5ca8d531593ae5b991a067cfcbdf8bf5a5bd18
AR-May <67507805+AR-May@users.noreply.github.com>
2021-08-23 19:17:02 +0200

    Add comparison for DirectoryEnumeration results.

--- src/Shared/FileSystem/ManagedFileSystem.cs
+++ src/Shared/FileSystem/ManagedFileSystem.cs
@@ -5,6 +5,7 @@ using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Microsoft.Build.Shared.FileSystem
 {
@@ -39,9 +40,47 @@ namespace Microsoft.Build.Shared.FileSystem
             return File.ReadAllBytes(path);
         }
 
+        private bool ResultsTheSame(IEnumerable<string> result0, IEnumerable<string> result1)
+        {
+            return result0.SequenceEqual(result1);
+        }
+
         public virtual IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOption)
         {
 #if FEATURE_MSIOREDIST
+            string ex0 = "", ex1 = "";
+            IEnumerable<string> result0 = null, result1 = null;
+
+            try
+            {
+                result0 = Directory.EnumerateFiles(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex0 = ex.Message;
+            }
+
+            try
+            {
+                result1 = ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
+                    ? Microsoft.IO.Directory.EnumerateFiles(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
+                    : Directory.EnumerateFiles(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex1 = ex.Message;
+            }
+
+            if (ex0 != ex1 || (ex0 == "" && !ResultsTheSame(result0, result1)))
+            {
+                string res0 = result0 == null ? result0.Aggregate((x, y) => x + ", " + y) : "-";
+                string res1 = result1 == null ? result1.Aggregate((x, y) => x + ", " + y) : "-";
+
+                string error = $"Error in EnumerateFiles: path: {path}, searchPattern: {searchPattern}, searchOption: {searchOption}.\nex0: {ex0}\nresult0: {res0}\nex1: {ex1}\nresult1: {res1}";
+                throw new InvalidOperationException(error);
+            }
+
+
             return ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
                     ? Microsoft.IO.Directory.EnumerateFiles(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
                     : Directory.EnumerateFiles(path, searchPattern, searchOption);
@@ -53,6 +92,39 @@ namespace Microsoft.Build.Shared.FileSystem
         public virtual IEnumerable<string> EnumerateDirectories(string path, string searchPattern, SearchOption searchOption)
         {
 #if FEATURE_MSIOREDIST
+            string ex0 = "", ex1 = "";
+            IEnumerable<string> result0 = null, result1 = null;
+
+            try
+            {
+                result0 = Directory.EnumerateDirectories(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex0 = ex.Message;
+            }
+
+            try
+            {
+                result1 = ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
+                    ? Microsoft.IO.Directory.EnumerateDirectories(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
+                    : Directory.EnumerateDirectories(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex1 = ex.Message;
+            }
+
+            if (ex0 != ex1 || (ex0 == "" && !ResultsTheSame(result0, result1)))
+            {
+                string res0 = result0 == null ? result0.Aggregate((x, y) => x + ", " + y) : "-";
+                string res1 = result1 == null ? result1.Aggregate((x, y) => x + ", " + y) : "-";
+
+                string error = $"Error in EnumerateDirectories: path: {path}, searchPattern: {searchPattern}, searchOption: {searchOption}.\nex0: {ex0}\nresult0: {res0}\nex1: {ex1}\nresult1: {res1}";
+                throw new InvalidOperationException(error);
+            }
+
+
             return ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
                     ? Microsoft.IO.Directory.EnumerateDirectories(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
                     : Directory.EnumerateDirectories(path, searchPattern, searchOption);
@@ -64,6 +136,39 @@ namespace Microsoft.Build.Shared.FileSystem
         public virtual IEnumerable<string> EnumerateFileSystemEntries(string path, string searchPattern, SearchOption searchOption)
         {
 #if FEATURE_MSIOREDIST
+            string ex0 = "", ex1 = "";
+            IEnumerable<string> result0 = null, result1 = null;
+
+            try
+            {
+                result0 = Directory.EnumerateFileSystemEntries(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex0 = ex.Message;
+            }
+
+            try
+            {
+                result1 = ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
+                    ? Microsoft.IO.Directory.EnumerateFileSystemEntries(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
+                    : Directory.EnumerateFileSystemEntries(path, searchPattern, searchOption);
+            }
+            catch (Exception ex)
+            {
+                ex1 = ex.Message;
+            }
+
+            if (ex0 != ex1 || (ex0 == "" && !ResultsTheSame(result0, result1)))
+            {
+                string res0 = result0 == null ? result0.Aggregate((x, y) => x + ", " + y) : "-";
+                string res1 = result1 == null ? result1.Aggregate((x, y) => x + ", " + y) : "-";
+
+                string error = $"Error in EnumerateFileSystemEntries: path: {path}, searchPattern: {searchPattern}, searchOption: {searchOption}.\nex0: {ex0}\nresult0: {res0}\nex1: {ex1}\nresult1: {res1}";
+                throw new InvalidOperationException(error);
+            }
+
+
             return ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0)
                     ? Microsoft.IO.Directory.EnumerateFileSystemEntries(path, searchPattern, (Microsoft.IO.SearchOption)searchOption)
                     : Directory.EnumerateFileSystemEntries(path, searchPattern, searchOption);
